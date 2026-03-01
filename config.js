d7b75deae8e71658a08cd4db580e19b69d208cf7
Geyzerrrr <130137729+Geyzerrrr@users.noreply.github.com>
2026-03-01 12:25:04 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - конфигурация путей для GitHub Pages
+window.appConfig = {
+    // Замените на свои данные
+    baseUrl: 'https://geyzerrr.github.io/название-репозитория', // ПОЛНЫЙ URL вашего GitHub Pages
+    vkAppId: '123456789', // ID вашего VK приложения (когда получите)
+    
+    // Пути к файлам
+    paths: {
+        element: 'element.html',
+        grunt: 'grunt.html',
+        test: 'test.html'
+    }
+};
